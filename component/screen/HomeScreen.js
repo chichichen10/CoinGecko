e1d131ac290c40d00e905a09f8e4cfca318e2b25
CHEN Ying Chi <chichichen10@gmail.com>
2021-07-26 14:04:12 +0800

    market api added

--- component/screen/HomeScreen.js
+++ component/screen/HomeScreen.js
@@ -1,12 +1,38 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+
 
 function HomeScreen() {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    console.log(data);
+
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home from component!</Text>
+
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? <Text>Loading...</Text> :
+                (<View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+                    <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{data.title}</Text>
+                    <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10 }}>Articles:</Text>
+                    <FlatList
+                        data={data}
+                        keyExtractor={({ id }, index) => id}
+                        renderItem={({ item }) => (
+                            <Text>{item.name + ': ' + item.current_price}</Text>
+                        )}
+                    />
+                </View>
+                )}
         </View>
-    )
+    );
 }
 
 export default HomeScreen;
