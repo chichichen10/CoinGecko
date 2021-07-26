8695e7d80be467e5b00cd76bb6ab0e48b24dba0c
CHEN Ying Chi <chichichen10@gmail.com>
2021-07-26 18:46:48 +0800

    scratch for first screen without sort

--- component/screen/HomeScreen.js
+++ component/screen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 
 function HomeScreen() {
@@ -15,18 +15,35 @@ function HomeScreen() {
             .finally(() => setLoading(false));
     }, []);
 
+    const DevideLine = () => {
+        return (
+            <View style={{ height: 1, backgroundColor: 'skyblue' }} />
+        )
+    }
+
+
     return (
 
         <View style={{ flex: 1, padding: 24 }}>
             {isLoading ? <Text>Loading...</Text> :
                 (<View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-                    <Text style={{ fontSize: 18, color: 'green', textAlign: 'center' }}>{data.title}</Text>
-                    <Text style={{ fontSize: 14, color: 'green', textAlign: 'center', paddingBottom: 10 }}>Articles:</Text>
                     <FlatList
                         data={data}
                         keyExtractor={({ id }, index) => id}
+                        ItemSeparatorComponent={DevideLine}
+                        ListHeaderComponent={() => (
+                            <View style={{ backgroundColor: "white", flexDirection: "row", alignItems: "center" }}>
+                                <Text style={{ fontWeight: "bold", width: "33%", textAlign: "center" }}>Name</Text>
+                                <Text style={{ width: "33%", textAlign: "center" }}>Price</Text>
+                                <Text style={{ width: "33%", textAlign: "center" }}>Volumn</Text>
+                            </View>
+                        )}
                         renderItem={({ item }) => (
-                            <Text>{item.name + ': ' + item.current_price}</Text>
+                            <View style={{ backgroundColor: "#F0FBFC", flexDirection: "row", alignItems: "center" }}>
+                                <Text style={{ fontWeight: "bold", width: "33%", textAlign: "center" }}>{item.name}</Text>
+                                <Text style={{ width: "33%", textAlign: "center" }}>{item.current_price}</Text>
+                                <Text style={{ width: "33%", textAlign: "center" }}>{item.total_volume}</Text>
+                            </View>
                         )}
                     />
                 </View>
@@ -34,5 +51,6 @@ function HomeScreen() {
         </View>
     );
 }
+
 
 export default HomeScreen;
