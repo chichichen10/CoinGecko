965a3fd1aa72305a7483bd8734d25538803342d6
CHEN Ying Chi <chichichen10@gmail.com>
2021-08-02 16:16:36 +0800

    change chart kit

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,60 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, Dimensions, ActivityIndicator, Platform } from 'react-native';
-import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis } from "victory-native";
-import LoadingComponent from '../../src/components/LoadingComponent';
-import { API_Coins, API_Coins_MarketChart } from '../models/CoinGeckoAPIType';
-
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  ActivityIndicator,
+  Platform,
+} from "react-native";
+import {
+  VictoryLine,
+  VictoryChart,
+  VictoryTheme,
+  VictoryAxis,
+} from "victory-native";
+import LoadingComponent from "../../src/components/LoadingComponent";
+import { API_Coins, API_Coins_MarketChart } from "../models/CoinGeckoAPIType";
+import { LineChart } from "react-native-chart-kit";
 
 const DetailScreen = ({ navigation, route }) => {
-    const [isLoadingCoinData, setLoadingCoinData] = useState(true);
-    const [isLoadingMarketData, setLoadingMarketData] = useState(true);
-    const [coinData, setCoinData] = useState<API_Coins>(null);
-    const [marketData, setMarketData] = useState<API_Coins_MarketChart>(null);
-    // console.log(data);
-    const screenWidth = Dimensions.get("window").width;
+  const [isLoadingCoinData, setLoadingCoinData] = useState(true);
+  const [isLoadingMarketData, setLoadingMarketData] = useState(true);
+  const [coinData, setCoinData] = useState<API_Coins>(null);
+  const [marketData, setMarketData] = useState<API_Coins_MarketChart>(null);
+  // console.log(data);
+  const screenWidth = Dimensions.get("window").width;
 
-    useEffect(() => {
-        fetch('https://api.coingecko.com/api/v3/coins/' + route.params.id + '?localization=false')
-            .then((response) => response.json())
-            .then((json) => setCoinData(json))
-            .catch((error) => console.error(error))
-            .finally(() => setLoadingCoinData(false));
-        fetch('https://api.coingecko.com/api/v3/coins/' + route.params.id + '/market_chart?vs_currency=usd&days=1')
-            .then((response) => response.json())
-            .then((json) => setMarketData(json))
-            .catch((error) => console.error(error))
-            .finally(() => setLoadingMarketData(false));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/" +
+        route.params.id +
+        "?localization=false"
+    )
+      .then((response) => response.json())
+      .then((json) => setCoinData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoadingCoinData(false));
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/" +
+        route.params.id +
+        "/market_chart?vs_currency=usd&days=1"
+    )
+      .then((response) => response.json())
+      .then((json) => setMarketData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoadingMarketData(false));
+  }, []);
 
+  const timestampToString = (timestamp) => {
+    var date = new Date(timestamp);
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    return month + "/" + day + "\n" + hours + ":" + minutes.substr(-2);
+  };
+  const chartConfig = {
+    backgroundColor: "#198964",
+    backgroundGradientFrom: "#eff3ff",
+    backgroundGradientTo: "#efefef",
+    color: (opacity = 255) => `rgba(0, 0, 0, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+    propsForDots: {
+      r: "0",
+      strokeWidth: "2",
+      stroke: "#ffa726",
+    },
+  };
 
-
-    const timestampToString = (timestamp) => {
-        var date = new Date(timestamp);
-        var hours = date.getHours();
-        var minutes = "0" + date.getMinutes();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        return month + '/' + day + '\n' + hours + ':' + minutes.substr(-2);
-
-    }
-
-    return (
-        <View style={{ flex: 1, padding: 24 }}>
-            {isLoadingCoinData || isLoadingMarketData ? <LoadingComponent /> :
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text>Details of {coinData.name}</Text>
-                    <Text>NTD ${coinData.market_data.current_price.twd}</Text>
-                    <Text>USD ${coinData.market_data.current_price.usd}</Text>
-                    <View >
-                        <VictoryChart width={350} theme={VictoryTheme.material}>
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoadingCoinData || isLoadingMarketData ? (
+        <LoadingComponent />
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text>Details of {coinData.name}</Text>
+          <Text>NTD ${coinData.market_data.current_price.twd}</Text>
+          <Text>USD ${coinData.market_data.current_price.usd}</Text>
+          <View style={{ flex: 1 }}>
+            {/* <VictoryChart width={350} theme={VictoryTheme.material}>
                             <VictoryAxis dependentAxis />
 
                             <VictoryLine data={marketData.prices.map(x => ({ time: x[0], price: x[1] }))} x="time" y="price" interpolation="natural" />
                             <VictoryAxis tickCount={5} tickFormat={(t) => timestampToString(t)} />
-                        </VictoryChart>
-                    </View>
-                </View>
-            }</View>
-    );
-}
+                        </VictoryChart> */}
+            <LineChart
+              data={{
+                labels: ["24h", "18h", "12h", "6h"],
+                datasets: [
+                  {
+                    data: marketData.prices.map((x) => x[1]),
+                  },
+                ],
+              }}
+              width={screenWidth}
+              height={220}
+              chartConfig={chartConfig}
+              bezier
+            />
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
 export default DetailScreen;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,141 +1,248 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { FlatList, StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
-import { set } from 'react-native-reanimated';
-import LoadingComponent from '../components/LoadingComponent'
+import React, { useEffect, useState, useCallback } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { set } from "react-native-reanimated";
+import LoadingComponent from "../components/LoadingComponent";
 
 const Footer = React.memo((props: { isPulling: boolean }) => {
-    const { isPulling } = props;
+  const { isPulling } = props;
 
-    return (!isPulling ?
-        <Text style={{ width: "100%", textAlign: "center" }}>Pull for more</Text> :
-        <View>
-            <ActivityIndicator animating size="large" color="#0000ff" />
-        </View>
-    )
+  return !isPulling ? (
+    <Text style={{ width: "100%", textAlign: "center" }}>Pull for more</Text>
+  ) : (
+    <View>
+      <ActivityIndicator animating size="large" color="#0000ff" />
+    </View>
+  );
 });
 
 function HomeScreen({ navigation }) {
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
-    // const [moreData, setMoreData] = useState([]);
-    const [sortBy, setSortBy] = useState("price");
-    const [descending, setDescending] = useState(true);
-    const [refreshing, setRefreshing] = useState(false);
-    const [renderCount, setRenderCount] = useState(0);
-    const [isPulling, setPulling] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  // const [moreData, setMoreData] = useState([]);
+  const [sortBy, setSortBy] = useState("price");
+  const [descending, setDescending] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
+  const [renderCount, setRenderCount] = useState(0);
+  const [isPulling, setPulling] = useState(false);
 
-    useEffect(() => {
-        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_desc&per_page=25&page=1&sparkline=false')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => console.error(error))
-            .finally(() => setLoading(false));
-    }, []);
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=price_desc&per_page=25&page=1&sparkline=false"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-    useEffect(() => {
-        setLoading(true);
-        console.log("sortBy: " + sortBy + " decending" + descending);
-        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=' + sortBy + '_' + textOrder() + '&per_page=25&page=1&sparkline=false')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => console.error(error))
-            .finally(() => { setLoading(false); setRenderCount(0); });
-    }, [sortBy, descending]);
+  useEffect(() => {
+    setLoading(true);
+    console.log("sortBy: " + sortBy + " decending" + descending);
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=" +
+        sortBy +
+        "_" +
+        textOrder() +
+        "&per_page=25&page=1&sparkline=false"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setLoading(false);
+        setRenderCount(0);
+      });
+  }, [sortBy, descending]);
 
-    useEffect(() => {
-        if (isPulling)
-            fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=' + sortBy + '_' + textOrder() + '&per_page=25&page=' + (renderCount + 2) + '&sparkline=false')
-                .then((response) => response.json())
-                .then((json) => setData(data.concat(json)))
-                .catch((error) => console.error(error))
-                .finally(() => {
-                    setRenderCount(renderCount + 1);
-                    setPulling(false)
-                });
-    }, [isPulling]);
-    const fetchMore = useCallback(() => {
-        setPulling(true);
-        console.log("pulling");
-    }, [])
+  useEffect(() => {
+    if (isPulling)
+      fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=" +
+          sortBy +
+          "_" +
+          textOrder() +
+          "&per_page=25&page=" +
+          (renderCount + 2) +
+          "&sparkline=false"
+      )
+        .then((response) => response.json())
+        .then((json) => setData(data.concat(json)))
+        .catch((error) => console.error(error))
+        .finally(() => {
+          setRenderCount(renderCount + 1);
+          setPulling(false);
+        });
+  }, [isPulling]);
+  const fetchMore = useCallback(() => {
+    setPulling(true);
+    console.log("pulling");
+  }, []);
 
-    const refresh = useCallback(() => {
-        setRefreshing(true);
-        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=' + sortBy + '_' + textOrder() + '&per_page=25&page=1&sparkline=false')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => console.error(error))
-            .finally(() => {
-                setRefreshing(false);
-                setRenderCount(0);
-            });
+  const refresh = useCallback(() => {
+    setRefreshing(true);
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=" +
+        sortBy +
+        "_" +
+        textOrder() +
+        "&per_page=25&page=1&sparkline=false"
+    )
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setRefreshing(false);
+        setRenderCount(0);
+      });
+  }, []);
 
-    }, [])
+  const textArrow = useCallback(() => (descending ? "↓" : "↑"), [descending]);
+  const textOrder = useCallback(
+    () => (descending ? "desc" : "asc"),
+    [descending]
+  );
 
-    const textArrow = useCallback(() => descending ? "↓" : "↑", [descending]);
-    const textOrder = useCallback(() => descending ? "desc" : "asc", [descending]);
-
-    const changeOrder = (target) => {
-        if (sortBy == target) setDescending(!descending);
-        else {
-            setSortBy(target);
-            setDescending(true);
-        }
-
+  const changeOrder = (target) => {
+    if (sortBy == target) setDescending(!descending);
+    else {
+      setSortBy(target);
+      setDescending(true);
     }
+  };
 
-    // const renderFooter = () => {
-    //     return (!isPulling ?
-    //         <Text style={{ width: "100%", textAlign: "center" }}>Pull for more</Text> :
-    //         <View>
-    //             <ActivityIndicator animating size="large" />
-    //         </View>
-    //     )
-    // }
+  // const renderFooter = () => {
+  //     return (!isPulling ?
+  //         <Text style={{ width: "100%", textAlign: "center" }}>Pull for more</Text> :
+  //         <View>
+  //             <ActivityIndicator animating size="large" />
+  //         </View>
+  //     )
+  // }
 
-    const DevideLine = () => {
-        return (
-            <View style={{ height: 1, backgroundColor: 'skyblue' }} />
-        )
-    }
+  const DevideLine = () => {
+    return <View style={{ height: 1, backgroundColor: "skyblue" }} />;
+  };
 
-
-
-    return (
-
-        <View style={{ flex: 1 }}>
-            {isLoading ? <LoadingComponent /> :
-                (<View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-                    <FlatList
-                        data={data}
-                        keyExtractor={(item, index) => index.toString()}
-                        ItemSeparatorComponent={DevideLine}
-                        ListHeaderComponent={() => (
-                            <View style={{ backgroundColor: "white", flexDirection: "row", alignItems: "center", height: 50 }}>
-                                <TouchableOpacity onPress={() => changeOrder("id")} style={{ width: "25%" }}><Text style={{ fontWeight: sortBy == "id" ? "bold" : "normal", textAlign: "center" }}>Name {sortBy == "id" ? textArrow() : ""}</Text></TouchableOpacity>
-                                <TouchableOpacity onPress={() => changeOrder("price")} style={{ width: "25%" }}><Text style={{ fontWeight: sortBy == "price" ? "bold" : "normal", textAlign: "center" }}>Price {sortBy == "price" ? textArrow() : ""}</Text></TouchableOpacity>
-                                <TouchableOpacity onPress={() => changeOrder("volume")} style={{ width: "25%" }}><Text style={{ fontWeight: sortBy == "volume" ? "bold" : "normal", textAlign: "center" }}>Volume {sortBy == "volume" ? textArrow() : ""}</Text></TouchableOpacity>
-                            </View>
-                        )}
-                        renderItem={({ item }) => (
-                            <View style={{ backgroundColor: "#F0FBFC", flexDirection: "row", alignItems: "center" }}>
-                                <Text style={{ fontWeight: "bold", width: "25%", textAlign: "center" }}>{item.name}</Text>
-                                <Text style={{ width: "25%", textAlign: "center" }}>{item.current_price}</Text>
-                                <Text style={{ width: "25%", textAlign: "center" }}>{item.total_volume}</Text>
-                                <TouchableOpacity style={{ alignItems: "center", marginLeft: 10, padding: 10, backgroundColor: "gray" }} onPress={() => navigation.navigate("Detail", { id: item.id })}><Text>DETAIL</Text></TouchableOpacity>
-                            </View>
-                        )}
-                        onEndReachedThreshold={0.01}
-                        onEndReached={fetchMore}
-                        ListFooterComponent={() => <Footer isPulling={isPulling} />}
-                        onRefresh={refresh}
-                        refreshing={refreshing}
-                        contentContainerStyle={{ padding: 10 }}
-                    />
-                </View>
-                )}
+  return (
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        <LoadingComponent />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <FlatList
+            data={data}
+            keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={DevideLine}
+            ListHeaderComponent={() => (
+              <View
+                style={{
+                  backgroundColor: "white",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  height: 50,
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => changeOrder("id")}
+                  style={{ width: "25%" }}
+                >
+                  <Text
+                    style={{
+                      fontWeight: sortBy == "id" ? "bold" : "normal",
+                      textAlign: "center",
+                    }}
+                  >
+                    Name {sortBy == "id" ? textArrow() : ""}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => changeOrder("price")}
+                  style={{ width: "25%" }}
+                >
+                  <Text
+                    style={{
+                      fontWeight: sortBy == "price" ? "bold" : "normal",
+                      textAlign: "center",
+                    }}
+                  >
+                    Price {sortBy == "price" ? textArrow() : ""}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => changeOrder("volume")}
+                  style={{ width: "25%" }}
+                >
+                  <Text
+                    style={{
+                      fontWeight: sortBy == "volume" ? "bold" : "normal",
+                      textAlign: "center",
+                    }}
+                  >
+                    Volume {sortBy == "volume" ? textArrow() : ""}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                  backgroundColor: "#F0FBFC",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    width: "25%",
+                    textAlign: "center",
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text style={{ width: "25%", textAlign: "center" }}>
+                  {item.current_price}
+                </Text>
+                <Text style={{ width: "25%", textAlign: "center" }}>
+                  {item.total_volume}
+                </Text>
+                <TouchableOpacity
+                  style={{
+                    alignItems: "center",
+                    marginLeft: 10,
+                    padding: 10,
+                    backgroundColor: "gray",
+                  }}
+                  onPress={() => navigation.navigate("Detail", { id: item.id })}
+                >
+                  <Text>DETAIL</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            onEndReachedThreshold={0.01}
+            onEndReached={fetchMore}
+            ListFooterComponent={() => <Footer isPulling={isPulling} />}
+            onRefresh={refresh}
+            refreshing={refreshing}
+            contentContainerStyle={{ padding: 10 }}
+          />
         </View>
-    );
+      )}
+    </View>
+  );
 }
-
 
 export default HomeScreen;
