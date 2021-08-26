5c9fa72f287bd26277fd906f53818dcebf561b4e
CHEN Ying Chi <chichichen10@gmail.com>
2021-08-26 15:16:37 +0800

    add search function

--- src/screens/DrawerContent.tsx
+++ src/screens/DrawerContent.tsx
@@ -4,6 +4,8 @@ import {
 } from 'react-native';
 import { DrawerItem, useDrawerStatus } from '@react-navigation/drawer';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SearchBar from 'react-native-dynamic-search-bar';
+import { ApiCoins } from '../models/CoinGeckoAPIType';
 
 const styles = StyleSheet.create({
   test: {
@@ -16,24 +18,68 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: 'RobotoCondensed_400Regular',
   },
+  searchBar: {
+    marginTop: 50,
+  },
+  errorText: {
+    marginTop: 10,
+    fontSize: 18,
+    fontFamily: 'RobotoCondensed_400Regular',
+    marginLeft: 15,
+  },
+  blank: {
+    height: 10,
+  },
 });
 
 export default function DrawerContent({ navigation }) {
   const [data, setData] = useState([]);
   const [count, setCount] = useState(0);
-  const renderItem = ({ item }) => (
-    <DrawerItem
-      label={item.symbol.toUpperCase()}
-      onPress={() => navigation.navigate('Detail', { id: item.id })}
-      labelStyle={styles.labelText}
-      icon={({ size }) => (
-        <Image
-          source={{ uri: item.image }}
-          resizeMode="contain"
-          style={{ height: size, width: size }}
-        />
-      )}
-    />
+  const [search, setSearch] = useState('');
+  const [searchCoin, setSearchCoin] = useState<ApiCoins>(null);
+  const [isLoading, setLoading] = useState(false);
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <DrawerItem
+        label={item.symbol.toUpperCase()}
+        onPress={() => navigation.navigate('Detail', { id: item.id })}
+        labelStyle={styles.labelText}
+        icon={({ size }) => (
+          <Image
+            source={{ uri: item.image }}
+            resizeMode="contain"
+            style={{ height: size, width: size }}
+          />
+        )}
+      />
+    ),
+    [],
+  );
+
+  const SearchResult = useCallback(
+    () => ('error' in searchCoin ? (
+      <Text style={styles.errorText}> Coin not found!</Text>
+    ) : (
+      <DrawerItem
+        label={searchCoin.symbol.toUpperCase()}
+        onPress={() => navigation.navigate('Detail', { id: searchCoin.id })}
+        labelStyle={styles.labelText}
+        icon={({ size }) => (
+          <Image
+            source={{ uri: searchCoin.image.small }}
+            resizeMode="contain"
+            style={{ height: size, width: size }}
+          />
+        )}
+      />
+    )),
+    [searchCoin],
+  );
+
+  const SearchItem = useCallback(
+    () => (searchCoin ? <SearchResult /> : <View style={styles.blank} />),
+    [searchCoin],
   );
 
   const getLikeList = useCallback(async () => {
@@ -55,8 +101,62 @@ export default function DrawerContent({ navigation }) {
   }, [useDrawerStatus()]);
   const keyExtractor = useCallback((item, index) => index.toString(), []);
 
+  const updateSearch = useCallback((text) => {
+    setSearch(text);
+  }, []);
+
+  // not able to use due to API limit
+  // useEffect(() => {
+  //   if (search !== '') {
+  //     setLoading(true);
+  //     fetch(`https://api.coingecko.com/api/v3/coins/${search.toLowerCase()}?localization=false`)
+  //       .then((response) => response.json())
+  //       .then((json) => {
+  //         setSearchCoin(json);
+  //       })
+  //       .catch((error) => console.error(error))
+  //       .finally(() => setLoading(false));
+  //   } else setLoading(false);
+  // }, [search]);
+
+  useEffect(() => {
+    if (searchCoin) {
+      if ('error' in searchCoin) {
+        console.log('error');
+      } else {
+        console.log(searchCoin.name);
+      }
+    }
+  }, [searchCoin]);
+
+  const handleClear = useCallback(() => {
+    setSearchCoin(null);
+  }, []);
+
+  const handlePressSearch = useCallback(() => {
+    if (search !== '') {
+      setLoading(true);
+      fetch(`https://api.coingecko.com/api/v3/coins/${search.toLowerCase()}?localization=false`)
+        .then((response) => response.json())
+        .then((json) => {
+          setSearchCoin(json);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    } else setLoading(false);
+  }, [search]);
+
   return (
     <View>
+      <SearchBar
+        style={styles.searchBar}
+        placeholder="Search here"
+        onSearchPress={handlePressSearch}
+        onChangeText={updateSearch}
+        spinnerVisibility={isLoading}
+        onClearPress={handleClear}
+      />
+      <SearchItem />
       <Text style={styles.test}>Favorite</Text>
       <FlatList data={data} keyExtractor={keyExtractor} renderItem={renderItem} />
     </View>
